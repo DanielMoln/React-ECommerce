c1c3f6286e986f271350204d57df1b7f5eaca348
Daniel Molnar <molnard.work@gmail.com>
2023-04-04 08:44:36 +0200

    created authentication component alongside sign in form

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+} from "../../utils/firebase/firebase.utils";
+import Button from "../button/button.component";
+import FormInput from "../form-input/form-input.component";
+import "./sign-in-form.styles.scss";
+
+const defaultFormFields = {
+  email: "",
+  password: "",
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({
+      ...formFields,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(response);
+      resetFormFields();
+    } catch (error) {
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert("Incorrect password!");
+          break;
+        case "auth/user-not-found":
+          alert("User not found!");
+          break;
+        case "auth/too-many-requests":
+          alert(
+            "Too many tries, please for a while before trying to login again!"
+          );
+          break;
+      }
+    }
+  };
+
+  return (
+    <div className="sign-in-container">
+      <h2>Already have an acoount?</h2>
+      <span>Sign in with your email and password, or with google</span>
+
+      <form onSubmit={handleFormSubmit}>
+        <FormInput
+          label="Email"
+          value={email}
+          name="email"
+          type="text"
+          required
+          onChange={handleChange}
+        />
+        <FormInput
+          label="Password"
+          value={password}
+          name="password"
+          type="password"
+          required
+          onChange={handleChange}
+        />
+
+        <div className="buttons-container">
+          <Button type="submit">Sing In</Button>
+          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+            Sing in with Google
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+export default SignInForm;
